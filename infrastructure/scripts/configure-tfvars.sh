1fd576605c4539a9070b7dd2211d9d8fc6490bc4
Matheus Robert Lichtnow <matheusrobert8@gmail.com>
2021-12-15 21:33:48 -0300

    added environment vars configuration

---
+++ infrastructure/scripts/configure-tfvars.sh
@@ -0,0 +1,5 @@
+rm ./infrastructure/terraform.tfvars
+
+echo "dbname = '${DB_NAME}'" >> ./infrastructure/terraform.tfvars
+echo "dbuser = '${DB_USER}'" >> ./infrastructure/terraform.tfvars
+echo "dbpassword = '${DB_PASSWORD}'" >> ./infrastructure/terraform.tfvars
